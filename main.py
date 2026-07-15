e616a0bc3394af9852833891bd8710b2d41346c1
Aljwharah-codes <jeej3300@gmail.com>
2026-07-15 21:49:48 +0300

    Add password strength checker

--- main.py
+++ main.py
@@ -1,4 +1,27 @@
 password = input("Enter your password: ")
 
-print("Your password is:")
-print(password)
+score = 0
+
+if len(password) >= 8:
+    score += 1
+
+if any(char.isupper() for char in password):
+    score += 1
+
+if any(char.islower() for char in password):
+    score += 1
+
+if any(char.isdigit() for char in password):
+    score += 1
+
+special_characters = "!@#$%^&*()_+-=[]{}|;:',.<>?/"
+
+if any(char in special_characters for char in password):
+    score += 1
+
+if score <= 2:
+    print("Weak Password ❌")
+elif score == 3 or score == 4:
+    print("Medium Password ⚠️")
+else:
+    print("Strong Password ✅")
